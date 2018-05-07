c6ba7d37387a38dab382d4bc2e5523998b25e5ed
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-05-07 15:48:50 -0400

    Checking for length

--- pwdzer.js
+++ pwdzer.js
@@ -1,7 +1,11 @@
 // this is my JS file
 
 // read a password from console
+var password = process.argv[2]
 
 // analyze
+var goodLength = password.length > 6 ? true : false;
 
 // output a report
+console.log("Password to be analyzed: ",password)
+console.log("Analysis of good length: ",goodLength)
