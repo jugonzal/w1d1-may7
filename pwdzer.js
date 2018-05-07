39554f3625e99435d652b107395e70a3dfeb8f91
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-05-07 15:55:55 -0400

    added number inclusion support

--- pwdzer.js
+++ pwdzer.js
@@ -6,6 +6,15 @@ var password = process.argv[2]
 // analyze
 var goodLength = password.length > 6 ? true : false;
 
+var allNumber = "0123456789"
+var hasNumbers = false;
+for (i=0; i< password.length; i++) {
+  if (allNumber.includes(password[i])) {
+    hasNumbers = true;
+  }
+}
+
 // output a report
 console.log("Password to be analyzed: ",password)
 console.log("Analysis of good length: ",goodLength)
+console.log("Includes numbers: ", hasNumbers)
